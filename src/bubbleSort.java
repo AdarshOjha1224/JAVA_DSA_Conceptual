bddc1ab4aa940c58046600da112d2c33620bd756
adarsh <adarshojha1224@gmail.com>
2026-05-24 00:26:37 +0530

    Bubble Sort in DSA.

--- src/bubbleSort.java
+++ src/bubbleSort.java
@@ -1,16 +1,27 @@
 public class bubbleSort {
     public static void bSort(int[] nums){
         int n = nums.length;
-        int m = n ;
-        for(int i = 0 ; i > n ; i++){
-            while(m>0){
-
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n-i-1; j++){
+                if (nums[j] > nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp ;
+                }
             }
         }
     }
 
     public static void main(String[] args) {
         int[] nums = {4,7,14,19,2,17,10,11,1};
-
+        System.out.println("Before Sorting : ");
+        for(int ele : nums){
+            System.out.print(ele + " ");
+        }
+        bSort(nums);
+        System.out.println("\nAfter Sorting : ");
+        for(int ele : nums){
+            System.out.print(ele + " ");
+        }
     }
 }
