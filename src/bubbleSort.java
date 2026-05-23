86705a40a59f7ac3ebb776235a4678ef89790e9c
adarsh <adarshojha1224@gmail.com>
2026-05-23 23:53:33 +0530

    Bubble Sort in DSA.

---
+++ src/bubbleSort.java
@@ -0,0 +1,16 @@
+public class bubbleSort {
+    public static void bSort(int[] nums){
+        int n = nums.length;
+        int m = n ;
+        for(int i = 0 ; i > n ; i++){
+            while(m>0){
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,7,14,19,2,17,10,11,1};
+
+    }
+}
