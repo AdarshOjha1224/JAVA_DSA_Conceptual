275f5223659e51774ba148a76b222f202ecd211f
adarsh <adarshojha1224@gmail.com>
2026-06-02 23:58:34 +0530

    Recurusion Concept in DSA.

--- src/recursion.java
+++ src/recursion.java
@@ -1,12 +1,9 @@
 public class recursion {
-
     public static int fact(int x){
         if(x!=0) return x * fact(--x);
         return 1;
     }
-
     public static void main(String[] args) {
         System.out.println(fact(5));
     }
-
 }
