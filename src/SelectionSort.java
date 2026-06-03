48806fae13131c09a369fb860852f3ab13838b76
adarsh <adarshojha1224@gmail.com>
2026-06-03 23:58:46 +0530

    Selection Sort in DSA.

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -3,13 +3,11 @@ public class SelectionSort {
         int n = nums.length;
         int temp = 0 ;
         int minIndex = -1 ;
-
         for(int i = 0 ; i < n-1 ; i++){
             minIndex = i ;
             for(int j = i+1 ; j < n ; j++){
                 if(nums[minIndex] > nums[j]) minIndex = j ;
             }
-
             temp = nums[minIndex];
             nums[minIndex] = nums[i];
             nums[i] = temp ;
@@ -17,7 +15,6 @@ public class SelectionSort {
             for(int ele : nums) System.out.print(ele + " ");
         }
     }
-
     public static void main(String[] args) {
         int[] nums = {4,7,14,19,2,17,10,11,1};
         System.out.println("Before Sorting : ");
