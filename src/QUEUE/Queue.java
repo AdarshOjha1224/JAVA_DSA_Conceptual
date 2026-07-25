207bca1d6aac2ec7cf0c6a0e48b6322ff05640da
adarsh <adarshojha1224@gmail.com>
2026-07-25 23:19:58 +0530

    Queue implementation from scratch , Not using built-in Queue.

--- src/QUEUE/Queue.java
+++ src/QUEUE/Queue.java
@@ -8,28 +8,36 @@ public class Queue {
     private int[] arr = new int[5];
 
     public void enqueue(int data) {
+
         if(!isFull()) {
             rear = (rear + 1) % 5;
             arr[rear] = data;
             size++;
         }
+
         else System.out.println("Queue is FULL !");
+
     }
 
     public int dequeue(){
+
         if(isEmpty()) {
             throw new RuntimeException("Queue is Empty");
         }
+
         int data = arr[front];
         front = (front + 1) % 5;
         size--;
+
         return data;
     }
 
     public int peek(){
+
         if(isEmpty()) {
             throw new RuntimeException("Queue is Empty");
         }
+
         return arr[front];
     }
 
@@ -42,9 +50,12 @@ public class Queue {
     }
 
     public void show(){
+
         for(int i=front ; i<size ; i++) {
             System.out.print(arr[i] + " ");
         }
+
         System.out.println();
     }
+
 }
